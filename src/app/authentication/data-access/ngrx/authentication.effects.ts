c23e08f85fa093e1891ce2b17c27b7dff6e31e2d
Mint Missy <dawidkostka480@gmail.com>
2022-11-28 01:39:35 +0100

    don't lint style files, fix dependency circle

--- src/app/authentication/data-access/ngrx/authentication.effects.ts
+++ src/app/authentication/data-access/ngrx/authentication.effects.ts
@@ -1,13 +1,9 @@
 import { Actions } from '@ngrx/effects';
-import { AuthenticationModule } from 'src/app/authentication/authentication.module';
-import { AuthenticationService } from '../authentication.service';
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: AuthenticationModule,
-})
+@Injectable()
 export class AuthenticationEffects {
-  constructor(private actions$: Actions, private authenticationService: AuthenticationService) {}
+  constructor(private actions$: Actions) {}
 
   // actionName$ = this.actions$.pipe(ofType(class.actionName));
 }
